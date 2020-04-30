b2023b7a5f4ab334a962a5b9d5ac205468b1dc29
letol <tolomei.leonardo@gmail.com>
2020-04-30 16:46:23 +0200

    Section 2. completed

---
+++ src/main/java/it/polito/ai/es2/controllers/CourseController.java
@@ -0,0 +1,35 @@
+package it.polito.ai.es2.controllers;
+
+import it.polito.ai.es2.dtos.CourseDTO;
+import it.polito.ai.es2.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/API/courses")
+public class CourseController {
+
+    @Autowired
+    TeamService teamService;
+
+    @GetMapping({"", "/"})
+    List<CourseDTO> all() {
+        return teamService.getAllCourses();
+    }
+
+    @GetMapping("/{name}")
+    CourseDTO getOne(@PathVariable String name) {
+        Optional<CourseDTO> courseDTO = teamService.getCourse(name);
+        if (courseDTO.isPresent())
+            return courseDTO.get();
+        else throw new ResponseStatusException(HttpStatus.CONFLICT, "Course " + name + " not found!");
+    }
+}
