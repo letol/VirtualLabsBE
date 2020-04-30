af6f18b11b32a4b0e456c03c6437c2c8697918c6
letol <tolomei.leonardo@gmail.com>
2020-04-30 19:56:10 +0200

    Section 7. completed

--- src/main/java/it/polito/ai/es2/controllers/CourseController.java
+++ src/main/java/it/polito/ai/es2/controllers/CourseController.java
@@ -3,11 +3,19 @@ package it.polito.ai.es2.controllers;
 import it.polito.ai.es2.dtos.CourseDTO;
 import it.polito.ai.es2.dtos.StudentDTO;
 import it.polito.ai.es2.services.TeamService;
+import it.polito.ai.es2.services.TeamServiceException;
+import org.apache.tika.Tika;
+import org.apache.tika.metadata.Metadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -40,8 +48,8 @@ public class CourseController {
             return teamService.getEnrolledStudents(name).stream()
                     .map(ModelHelper::enrich)
                     .collect(Collectors.toList());
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (TeamServiceException tse) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, tse.getMessage());
         }
     }
 
@@ -51,5 +59,36 @@ public class CourseController {
             return ModelHelper.enrich(courseDTO);
         else
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Course '" + courseDTO.getName() + "' already exists!");
+    }
+
+    @PostMapping("/{name}/enrollOne")
+    Boolean enrollStudent(@PathVariable String name, @RequestParam("studentId") String studentId) {
+        try {
+            return teamService.addStudentToCourse(studentId, name);
+        } catch (TeamServiceException tse) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, tse.getMessage());
+        }
+    }
+
+    @PostMapping("/{name}/enrollMany")
+    List<Boolean> enrollStudents(@PathVariable String name, @RequestParam("file") MultipartFile file) {
+        Tika tika = new Tika();
+        Metadata meta = new Metadata();
+        meta.add(Metadata.RESOURCE_NAME_KEY, file.getOriginalFilename());
+        try {
+            String mimeType = tika.detect(file.getInputStream(), meta);
+            if (!mimeType.equals("text/csv"))
+                throw new ResponseStatusException(HttpStatus.UNSUPPORTED_MEDIA_TYPE, mimeType);
+        } catch (IOException ioe) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ioe.getMessage());
+        }
+
+        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            return teamService.addAndEnroll(reader, name);
+        } catch (TeamServiceException tse) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, tse.getMessage());
+        } catch (IOException ioe) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ioe.getMessage());
+        }
     }
 }
--- src/main/java/it/polito/ai/es2/controllers/StudentController.java
+++ src/main/java/it/polito/ai/es2/controllers/StudentController.java
@@ -4,10 +4,7 @@ import it.polito.ai.es2.dtos.StudentDTO;
 import it.polito.ai.es2.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -33,6 +30,13 @@ public class StudentController {
         Optional<StudentDTO> studentDTO = teamService.getStudent(id);
         if (studentDTO.isPresent())
             return ModelHelper.enrich(studentDTO.get());
-        else throw new ResponseStatusException(HttpStatus.CONFLICT, "Student id " + id + " not found!");
+        else throw new ResponseStatusException(HttpStatus.CONFLICT, "Student id '" + id + "' not found!");
+    }
+
+    @PostMapping({"", "/"})
+    StudentDTO addStudent(@RequestBody StudentDTO studentDTO) {
+        if (teamService.addStudent(studentDTO))
+            return ModelHelper.enrich(studentDTO);
+        else throw new ResponseStatusException(HttpStatus.CONFLICT, "Student id '" + studentDTO.getId() + "' already exists!");
     }
 }
