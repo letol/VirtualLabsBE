449812e2bc26cf371803c0334434d0b4d2cc4e47
Leonardo Tolomei <tolomei.leonardo@gmail.com>
2021-02-14 13:17:33 +0100

    transform NotificationController to @RestController

--- src/main/java/it/polito/ai/es2/controllers/NotificationController.java
+++ src/main/java/it/polito/ai/es2/controllers/NotificationController.java
@@ -1,16 +1,17 @@
 package it.polito.ai.es2.controllers;
 
-import it.polito.ai.es2.utility.ViewMessage;
+import it.polito.ai.es2.exceptions.TeamServiceException;
 import it.polito.ai.es2.services.NotificationService;
 import it.polito.ai.es2.exceptions.NotificationServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+@RestController
 @RequestMapping("/API/notifications")
 public class NotificationController {
 
@@ -18,44 +19,20 @@ public class NotificationController {
     NotificationService notificationService;
 
     @GetMapping("/confirm/{t}")
-    public String confirmToken(Model model, @PathVariable("t") String tokenId) {
+    public boolean confirmToken(@PathVariable("t") String tokenId) {
         try {
-            notificationService.confirm(tokenId);
-            ViewMessage msg = ViewMessage.builder()
-                    .title("Participation confirmed")
-                    .message("Your participation is now confirmed!")
-                    .borderColour("blue")
-                    .build();
-            model.addAttribute("message", msg);
-        } catch (NotificationServiceException nse) {
-            ViewMessage msg = ViewMessage.builder()
-                    .title("Invalid Token")
-                    .message("Ops! Something went wrong!")
-                    .borderColour("red")
-                    .build();
-            model.addAttribute("message", msg);
+            return notificationService.confirm(tokenId);
+        } catch (NotificationServiceException | TeamServiceException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
-        return "token";
     }
 
     @GetMapping("/reject/{t}")
-    public String rejectToken(Model model, @PathVariable("t") String tokenId) {
+    public boolean rejectToken(@PathVariable("t") String tokenId) {
         try {
-            notificationService.reject(tokenId);
-            ViewMessage msg = ViewMessage.builder()
-                    .title("Participation rejected")
-                    .message("Your participation is now rejected.")
-                    .borderColour("black")
-                    .build();
-            model.addAttribute("message", msg);
-        } catch (NotificationServiceException nse) {
-            ViewMessage msg = ViewMessage.builder()
-                    .title("Invalid Token")
-                    .message("Ops! Something went wrong!")
-                    .borderColour("red")
-                    .build();
-            model.addAttribute("message", msg);
+            return notificationService.reject(tokenId);
+        } catch (NotificationServiceException | TeamServiceException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
-        return "token";
     }
 }
