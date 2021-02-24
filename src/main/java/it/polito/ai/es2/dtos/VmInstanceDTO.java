2b5bd35cc703c7126f485989676a3e28f7c43c09
Hamza Rhaouati <44225903+ReddaHawk@users.noreply.github.com>
2021-02-24 22:56:59 +0100

    Update VmInstanceDTO.java

--- src/main/java/it/polito/ai/es2/dtos/VmInstanceDTO.java
+++ src/main/java/it/polito/ai/es2/dtos/VmInstanceDTO.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Min;
 
 @Data
 @Builder
